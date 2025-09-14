e93a4ac3ec422e8ed40ccc686c8b53c1014235c1
VivaanP123 <s32406@caseygrammar.vic.edu.au>
2025-09-14 23:30:23 +0000

    end of period 1

--- Task3.py
+++ Task3.py
@@ -3,7 +3,11 @@
 # Write a program that asks the user
 def main():
     #Write your code for input here
-    
+    dog_years = int(input("Dog years: "))
+ 
+    human_years = dog_years * 7 
+    print(f"Human age = {human_years}") 
+
 
 
     # End of your code for input here
